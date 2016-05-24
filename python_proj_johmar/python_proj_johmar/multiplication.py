f27c4aa5ac1e8e3475a7ed5df1aa32d423768655
vonDassendorf <chstoffesson@gmail.com>
2016-05-24 20:49:16 +0200

    added start of multi

---
+++ python_proj_johmar/python_proj_johmar/multiplication.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+
+
+def multiplication():
+    multiwin = Tk()
+    multiwin.title("Mulptiplication exercises")
+    window.vm_iconbitmap("mulicon.ico")
+    canvas = Canvas(multiwin)
+    window.mainloop()
